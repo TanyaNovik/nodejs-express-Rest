f3aa0cea8f196d228610688351b423e7ca69cf29
Tatsiana Novik <Tatsiana_Novik@epam.com>
2021-05-14 21:33:23 +0300

    feat: add users routs

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,25 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+
+const allUsers = [];
+const getAll = async () => allUsers;
+const getById = async (id) => allUsers.find(user => user.id === id);
+
+const save = async (name, login, password) => {
+  const newUser = new User({name, login, password});
+  allUsers.push(newUser);
+  return newUser;
 };
 
-module.exports = { getAll };
+const update = async (id, name, login, password) => {
+  const needUser = allUsers.find(user => user.id === id);
+  needUser.name = name;
+  needUser.login = login;
+  needUser.password = password;
+  return needUser;
+}
+
+const deleteUser = async (id) => {
+  const index = allUsers.findIndex(user => user.id === id);
+  allUsers.splice(index, 1);
+}
+module.exports = { getAll, getById, save, update, deleteUser };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,29 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.getUser(req.params.id);
+  res.json(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+  const {name, login, password} = req.body;
+  const user = await usersService.save(name, login, password);
+  res.status(201).send(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const {name, login, password} = req.body;
+  const user = await usersService.update(req.params.id, name, login, password);
+  res.status(200).send(User.toResponse(user));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await usersService.deleteUser(req.params.id);
+  res.status(204).send('The user has been deleted');
 });
 
 module.exports = router;
