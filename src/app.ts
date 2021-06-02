65a617d78a12e621b539bb4eb2cbca408363fe0b
Tatsiana Novik <Tatsiana_Novik@epam.com>
2021-06-02 22:19:45 +0300

    feat: add log uncaughtException and uncaughtRejection, delete morgan and write file log

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,32 @@
-import { createWriteStream } from 'fs';
+import { writeFileSync } from 'fs';
 import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
 import { finished } from 'stream';
-import morgan from 'morgan';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
 
+interface IReason{
+  message:string;
+}
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-app.use(morgan('dev', {stream: createWriteStream('./access.log')}))
+
+process.on('unhandledRejection', (reason:IReason) => {
+  console.error(`Unhandled rejection detected: ${reason.message}`);
+});
+
+process.on('uncaughtException', (error:Error) => {
+  console.error(`Captured error: ${error.message}`);
+  process.exit(1);
+});
+
 app.use('/', (req, res, next) => {
   const { method, url, body} = req;
   if (req.originalUrl === '/') {
@@ -26,6 +37,7 @@ app.use('/', (req, res, next) => {
 
   finished(res, () => {
     const {statusCode} = res;
+    writeFileSync('./out.log', `\n method: ${method} url: ${url} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(body)} statusCode: ${statusCode}`, {flag: 'a'})
     console.log(`method: ${method} url: ${url} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(body)} statusCode: ${statusCode}`)
   })
 });
