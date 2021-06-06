b39e802d1d29038f555102f71b58cbd18cc19f21
Tatsiana Novik <Tatsiana_Novik@epam.com>
2021-06-06 11:08:43 +0300

    feat: add write error message to file and uncaught error handler

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import { writeFileSync } from 'fs';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
 import { finished } from 'stream';
+import { getStatusText, INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
 
+
 interface IReason{
   message:string;
 }
+
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
@@ -20,10 +23,12 @@ app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 process.on('unhandledRejection', (reason:IReason) => {
   console.error(`Unhandled rejection detected: ${reason.message}`);
+  writeFileSync('./error.log', `Unhandled rejection detected: ${reason.message}`, {flag: 'a'})
 });
 
 process.on('uncaughtException', (error:Error) => {
   console.error(`Captured error: ${error.message}`);
+  writeFileSync('./error.log', `Captured error: ${error.message}`, {flag: 'a'})
   process.exit(1);
 });
 
@@ -33,17 +38,21 @@ app.use('/', (req, res, next) => {
     res.send('Service is running!');
     return;
   }
-  next();
 
   finished(res, () => {
     const {statusCode} = res;
     writeFileSync('./out.log', `\n method: ${method} url: ${url} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(body)} statusCode: ${statusCode}`, {flag: 'a'})
     console.log(`method: ${method} url: ${url} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)} body: ${JSON.stringify(body)} statusCode: ${statusCode}`)
   })
+  next();
+
 });
 
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
-
+app.use((err:Error, _req:Request, res:Response) => {
+  res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
+  writeFileSync('./error.log', `Captured error: ${err.message}`, {flag: 'a'});
+});
 export default app;
